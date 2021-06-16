1710c51a8fec8a1d68f2ebf19a27767d4c3b6a43
Xevro <29582386+Xevro@users.noreply.github.com>
2021-06-16 12:45:07 +0200

    added the train & eval data

--- trainer/model.py
+++ trainer/model.py
@@ -6,19 +6,28 @@ This file contains all the model information: the training steps, the batch
 size and the model itself.
 """
 
+BATCH_SIZE = 50
+NUM_TRAINING_STEPS = 10 #20000
+
+
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras import layers
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
 
 def get_batch_size():
     """Returns the batch size that will be used by your solution.
     It is recommended to change this value.
     """
-    return 1
+    return BATCH_SIZE
 
 def get_epochs():
     """Returns number of epochs that will be used by your solution.
     It is recommended to change this value.
     """
-    return 1
+    return NUM_TRAINING_STEPS
 
 def solution(input_layer):
     """Returns a compiled model.
@@ -39,7 +48,77 @@ def solution(input_layer):
     """
 
     # TODO: Code of your solution
-    model = None
+
+    """model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(64, 64, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(5)])
+
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+    """
+        
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=(4, 4), activation="relu", input_shape=(64, 64, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #.add(Dropout(0.25))
+    model.add(Conv2D(filters=32, kernel_size=(4, 4), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    model.add(Conv2D(filters=64, kernel_size=(4, 4), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    model.add(Conv2D(filters=64, kernel_size=(4, 4), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(25, activation='sigmoid'))
+
+    model.summary()
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # TODO: Return the compiled model
     return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
