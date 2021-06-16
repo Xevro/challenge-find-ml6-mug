e61b69270abb6dfc80e7995f8d5142c9648e0afa
Xevro <29582386+Xevro@users.noreply.github.com>
2021-06-16 17:02:11 +0200

    added a model to the solution function

--- trainer/model.py
+++ trainer/model.py
@@ -35,7 +35,7 @@ def solution(input_layer):
     This function is expected to return a model to identity the different mugs.
     The model's outputs are expected to be probabilities for the classes and
     and it should be ready for training.
-    The input layer specifies the shape of the images. The preprocessing
+    The input layer specifies    the shape of the images. The preprocessing
     applied to the images is specified in data.py.
 
     Add your solution below.
@@ -48,46 +48,20 @@ def solution(input_layer):
     """
 
     # TODO: Code of your solution
-
-    """model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(64, 64, 3)),
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.2),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(5)])
-
-    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-    """
         
     model = Sequential()
-    model.add(Conv2D(filters=16, kernel_size=(4, 4), activation="relu", input_shape=(64, 64, 3)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #.add(Dropout(0.25))
-    model.add(Conv2D(filters=32, kernel_size=(4, 4), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
-    model.add(Conv2D(filters=64, kernel_size=(4, 4), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
-    model.add(Conv2D(filters=64, kernel_size=(4, 4), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
+    #model.add(Conv2D(filters=16, kernel_size=(4, 4), activation="relu", input_shape=(64, 64, 3)))
+    model.add(Flatten(input_shape=(64, 64, 3))) 
+    model.add(Dense(16, activation=layers.LeakyReLU(alpha=0.3))) 
+    model.add(Dropout(0.6))
+    model.add(Dense(32, activation=layers.LeakyReLU(alpha=0.3))) 
     model.add(Dropout(0.5))
-    model.add(Dense(25, activation='sigmoid'))
+    model.add(Dense(64, activation=layers.LeakyReLU(alpha=0.3))) 
+    model.add(Dropout(0.3))
+    #model.add(Flatten())
+    model.add(Dense(4, activation='softmax'))
 
-    model.summary()
-
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
     # TODO: Return the compiled model
     return model
